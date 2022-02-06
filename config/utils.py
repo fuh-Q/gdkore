bbb4ad82db264238f9348935f45175308fc2dcfa
GDKID <bawbeefarted@gmail.com>
2022-02-06 01:51:24 -0500

    add mobile status magik to utils

--- config/utils.py
+++ config/utils.py
@@ -1,5 +1,6 @@
 import datetime
 import re
+import sys
 from typing import Optional, SupportsInt, Union
 
 import discord
@@ -82,6 +83,43 @@ def humanize_timedelta(
             strings.append(f"{period_value} {unit}")
 
     return ", ".join(strings)
+
+
+async def mobile(self: discord.gateway.DiscordWebSocket):
+    payload = {
+        "op": self.IDENTIFY,
+        "d": {
+            "token": self.token,
+            "properties": {
+                "$os": sys.platform,
+                "$browser": "Discord iOS",
+                "$device": "pycord",
+                "$referrer": "",
+                "$referring_domain": ""
+            },
+            "compress": True,
+            "large_threshold": 250,
+            "v": 3
+        }
+    }
+    
+    if self.shard_id is not None and self.shard_count is not None:
+        payload["d"]["shard"] = [self.shard_id, self.shard_count]
+    
+    state = self._connection
+    if state._activity is not None or state._status is not None:
+        payload["d"]["presence"] = {
+            "status": state._status,
+            "game": state._activity,
+            "since": 0,
+            "afk": False
+        }
+    
+    if state._intents is not None:
+        payload["d"]["intents"] = state._intents.value
+    
+    await self.call_hooks("before_identify", self.shard_id, initial=self._initial_identify)
+    await self.send_as_json(payload)
 
 
 class Botcolours:
