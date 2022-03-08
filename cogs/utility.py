f1deaadadf5e25a5f96d3fc6e1ca6ecb9cb24c2a
GDKID <bawbeefarted@gmail.com>
2022-03-08 01:15:18 -0500

    forgot ephemeral

--- cogs/utility.py
+++ cogs/utility.py
@@ -132,10 +132,10 @@ class Utility(commands.Cog):
         output = markdownify(text)
         
         try:
-            await ctx.respond(f"```{output}```\n**Copy paste the stuff in the codeblock above**")
+            await ctx.respond(f"```{output}```\n**Copy paste the stuff in the codeblock above**", ephemeral=True)
             
         except discord.HTTPException:
-            await ctx.respond("Something went wrong, most likely the output exceeded my character limit in sending messages or smth like that")
+            await ctx.respond("Something went wrong, most likely the output exceeded my character limit in sending messages or smth like that", ephemeral=True)
 
 
 def setup(client: commands.Bot):
