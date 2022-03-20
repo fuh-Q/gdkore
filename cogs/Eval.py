f62b7d86a89a2e79ac84a7f1a09ec5ed6145d208
GDKID <bawbeefarted@gmail.com>
2022-03-20 02:18:23 -0400

    add eval back

--- cogs/Eval.py
+++ cogs/Eval.py
@@ -5,6 +5,7 @@ import io
 import os
 import random
 import re
+import textwrap
 import traceback
 import types
 from contextlib import redirect_stdout
@@ -417,6 +418,105 @@ class Eval(BattlerCog):
                     content="Bro you're supposed to end these sessions once you're done wipe your own ass for once"
                 )
                 return
+    
+    @repl.command(name="noreturn", aliases=["nr"], brief="Runs code", hidden=True)
+    @commands.is_owner()
+    async def _eval(self, ctx: commands.Context, *, code: str):
+        env = self.get_environment(ctx)
+        
+        env.update(globals())
+
+        body = self.cleanup_code(body)
+        stdout = io.StringIO()
+
+        to_compile = f'async def func():\n{textwrap.indent(body, "    ")}'
+
+        def paginate(text: str):
+            '''Simple generator that paginates text.'''
+            last = 0
+            pages = []
+            for curr in range(0, len(text)):
+                if curr % 1980 == 0:
+                    pages.append(text[last:curr])
+                    last = curr
+                    appd_index = curr
+            if appd_index != len(text)-1:
+                pages.append(text[last:curr])
+            return list(filter(lambda a: a != '', pages))
+        
+        color = 0x2e3135
+
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            embed=discord.Embed(
+                title='Error',
+                description=f'```py\n{e.__class__.__name__}: {e}\n```',
+                color=color
+            )
+            await ctx.send(embed=embed)
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception:
+            value = stdout.getvalue()
+            embed=discord.Embed(
+                title='Error',
+                description=f'```py\n{value}{traceback.format_exc()}\n```',
+                color=color
+            )
+            await ctx.send(embed=embed)
+        else:
+            value = stdout.getvalue()
+            if ret is None:
+                if value:
+                    try:
+                        embed=discord.Embed(
+                            description=f'```py\n{value}\n```',
+                                color=color
+                            )
+                        await ctx.send(embed=embed)
+                    except:
+                        paginated_text = paginate(value)
+                        for page in paginated_text:
+                            if page == paginated_text[-1]:
+                                embed=discord.Embed(
+                                    description=f'```py\n{page}\n```',
+                                    color=color
+                                    )
+                                await ctx.send(embed=embed)
+                                break
+                            embed=discord.Embed(
+                                description=f'```py\n{page}\n```',
+                                color=color
+                                )
+                            await ctx.send(embed=embed)
+            else:
+                try:
+                    embed=discord.Embed(
+                        description=f'```py\n{value}{ret}\n```',
+                        color=color
+                        )
+                    await ctx.send(embed=embed)
+                except:
+                    paginated_text = paginate(f"{value}{ret}")
+                    for page in paginated_text:
+                        if page == paginated_text[-1]:
+                            embed=discord.Embed(
+                                description=f'```py\n{page}\n```',
+                                color=color
+                                )
+                            await ctx.send(embed=embed)
+                            break
+                        embed=discord.Embed(
+                            description=f'```py\n{page}\n```',
+                            color=color
+                        )
+                        await ctx.send(embed=embed)
+            
+            await ctx.message.add_reaction(self.client.yes)
 
     @repl.command(name="exec", aliases=["run"], brief="Runs code", hidden=True)
     @commands.is_owner()
