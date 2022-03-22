a26c0b49530ef6bc52aa6742aff8576ce6c924a1
GDKID <bawbeefarted@gmail.com>
2022-03-22 19:24:51 -0400

    fix context menu

--- cogs/utility.py
+++ cogs/utility.py
@@ -173,7 +173,7 @@ class Utility(commands.Cog):
         if not member.bot:
             return await ctx.respond(f"{member.mention} is not a bot", ephemeral=True)
 
-        url = f"https://discord.com/oauth2/authorize?client_id={member.id}&permissions=1099511627775&scope=bot%20applications.commands"
+        url = f"https://discord.com/oauth2/authorize?client_id={member.id}&permissions=543312838143&scope=bot%20applications.commands"
 
         return await ctx.respond(
             f"[click here to invite {member.name}]({url}) (feel free to toggle the invite's permissions as needed)",
