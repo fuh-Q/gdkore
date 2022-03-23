be9f3ab234084b3fed7ceae1921dd3af80a1b90b
{{ github.actor }} <GDKID69@users.noreply.github.com>
2022-03-23 03:31:22 +0000

    Style Reformatting

--- cogs/tts.py
+++ cogs/tts.py
@@ -1,12 +1,13 @@
-import discord
-from discord.ext import commands
-from discord.commands import slash_command, ApplicationContext, Option, CommandPermission
-
-from bot import NotGDKID
-
-from gtts import gTTS
 import os
 import sys
+
+import discord
+from discord.commands import (ApplicationContext, CommandPermission, Option,
+                              slash_command)
+from discord.ext import commands
+from gtts import gTTS
+
+from bot import NotGDKID
 
 
 class TTS(commands.Cog):
@@ -16,33 +17,27 @@ class TTS(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print("TTS cog loaded")
-    
+
     @slash_command(
         guild_ids=[890355226517860433],
         default_permissions=False,
         permissions=[CommandPermission(id=596481615253733408, type=2, permission=True)],
     )
-    async def tts(
-        self,
-        ctx: ApplicationContext,
-        text: Option(
-            str,
-            "what i'll say",
-            required=True
-        )
-    ):
+    async def tts(self, ctx: ApplicationContext, text: Option(str, "what i'll say", required=True)):
         """say something"""
         await ctx.interaction.response.defer(ephemeral=True)
         if ctx.voice_client and not ctx.voice_client.is_playing():
             try:
                 gTTS(text=text, lang="en", slow=False).save(fp := f"tts.mp3")
-            
+
             except Exception as e:
                 print(e)
                 return await ctx.respond("something went wrong there", ephemeral=True)
-            
-            src = discord.FFmpegPCMAudio(source=fp, executable=r"/usr/bin/ffmpeg" if sys.platform == "linux" else r"d:\thingyy\ffmpeg.exe")
-            ctx.voice_client.play(src, after = lambda _: os.remove("tts.mp3"))
+
+            src = discord.FFmpegPCMAudio(
+                source=fp, executable=r"/usr/bin/ffmpeg" if sys.platform == "linux" else r"d:\thingyy\ffmpeg.exe"
+            )
+            ctx.voice_client.play(src, after=lambda _: os.remove("tts.mp3"))
 
 
 def setup(client: NotGDKID):
