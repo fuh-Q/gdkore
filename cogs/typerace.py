fde3767105e03afd73ddbf1cfe5bdefa592b6ad7
GDKID <bawbeefarted@gmail.com>
2022-03-29 19:41:29 -0400

    .

---
+++ cogs/typerace.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+from discord.commands import (
+    slash_command,
+    ApplicationContext
+)
+
+from bot import NotGDKID
+
+class TypeRace(commands.Cog):
+    def __init__(self, client: NotGDKID) -> None:
+        self.client = client
+    
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Typerace cog loaded")
+    
+    @commands.command(aliases=["tr"])
+    @commands.is_owner()
+    async def typerace(self, ctx: commands.Context):
+        """starts a typerace"""
+
+
+def setup(client: NotGDKID):
+    client.add_cog(TypeRace(client=client))
