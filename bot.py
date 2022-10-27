358842583f540573330238c82abfb664d5c8bdb7
GDKID <bawbeefarted@gmail.com>
2022-10-26 20:56:00 -0400

    Remove topics scope

--- bot.py
+++ bot.py
@@ -68,7 +68,6 @@ class GClass(commands.Bot):
         "https://www.googleapis.com/auth/classroom.courses.readonly",
         "https://www.googleapis.com/auth/classroom.courseworkmaterials.readonly",
         "https://www.googleapis.com/auth/classroom.student-submissions.me.readonly",
-        "https://www.googleapis.com/auth/classroom.topics.readonly",
         "https://www.googleapis.com/auth/classroom.push-notifications",
     ]
     
