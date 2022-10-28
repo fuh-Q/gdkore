0b31075e0298629ef1c85cec149814fafdc21862
GDKID <bawbeefarted@gmail.com>
2022-10-27 20:22:10 -0400

    Ditch pub/sub

--- bot.py
+++ bot.py
@@ -67,7 +67,7 @@ class GClass(commands.Bot):
         "https://www.googleapis.com/auth/classroom.announcements.readonly",
         "https://www.googleapis.com/auth/classroom.courses.readonly",
         "https://www.googleapis.com/auth/classroom.courseworkmaterials.readonly",
-        "https://www.googleapis.com/auth/classroom.student-submissions.me.readonly",
+        "https://www.googleapis.com/auth/classroom.student-submissions.me.readonly"
     ]
     
     logger = logging.getLogger(__name__)
