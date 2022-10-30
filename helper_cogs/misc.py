ce3fb2abf806364f7e18c6300bf9d9f4b02a1a59
GDKID <bawbeefarted@gmail.com>
2022-10-30 18:43:14 -0400

    remove flip off

--- helper_cogs/misc.py
+++ helper_cogs/misc.py
@@ -18,36 +18,6 @@ if TYPE_CHECKING:
 class Misc(commands.Cog):
     def __init__(self, client: NotGDKID) -> None:
         self.client = client
-        
-        self.client.tree.add_command(ContextMenu(
-            name="flip them off",
-            callback=self.middle_finger
-        ))
-    
-    async def cog_unload(self) -> None:
-        self.client.tree.remove_command("flip them off")
-    
-    @guild_only()
-    async def middle_finger(self, interaction: Interaction, user: discord.User):
-        def runner(avatar: bytes) -> discord.File:
-            with BytesIO(avatar) as buffer:
-                with Image.open(buffer) as pfp:
-                    with Image.open("assets/finger.png").convert("RGBA") as finger:
-                        pfp.paste(finger, (138, pfp.height - finger.height), finger)
-                        buffer.seek(0)
-                        pfp.save(buffer, "png")
-                        buffer.seek(0)
-            
-                return discord.File(buffer, "fu.png")
-        
-        await interaction.response.defer()
-        content = f"{user.mention} {interaction.user.display_name} told me to flip you off"
-        avatar = interaction.user.avatar.with_size(256)
-        
-        await interaction.followup.send(
-            content=content,
-            file=await asyncio.to_thread(runner, await avatar.read())
-        )
     
     @command(name="shoppingcart")
     async def _shoppingcart(self, interaction: Interaction):
